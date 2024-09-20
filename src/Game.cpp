98636b17ea7489bac50cf61d5fe2b132078225ce
Matthew Hale <Matth3w4000@gmail.com>
2024-09-20 13:15:11 +0100

    Functions in header file, added Game.cpp

---
+++ src/Game.cpp
@@ -0,0 +1,26 @@
+#include "Game.h"
+
+Game::Game(unsigned int width, unsigned int height, unsigned int length) {
+}
+
+Game::~Game() {
+}
+
+void Game::init() {
+}
+
+void Game::processInput(float dt) {
+}
+
+void Game::update(float dt) {
+}
+
+void Game::stepGame() {
+}
+
+bool Game::getNewCherry(std::vector<Snake>& snakes) {
+    return false;
+}
+
+void Game::reset() {
+}
